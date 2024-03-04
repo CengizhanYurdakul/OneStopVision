83d4d3ab808e1c284c4341aabadcc807c7d42ac8
CengizhanYurdakul <cengizhanyurdakul97@gmail.com>
2024-03-04 23:47:15 +0300

    Downloader script added

---
+++ downloadModels.sh
@@ -0,0 +1,11 @@
+# Age-Gender
+gdown --id 1TGy6IZ2uOGrxdMYePBHWtfLFDA_q0r7a -O src/Models/
+
+# Head Pose
+gdown --id 1cRHNZU2-sb5eMn0RauCVjubIZzXaiGEP -O src/Models/
+
+# Face Parsing
+gdown --id 1jYZOMFTJ3HSjy7YDTRgN8Zm5RBDEw8hD -O src/Models/
+
+# Face Recognition
+gdown --id 1y92lHNJrhJOLe3133l58tki0VJLKNbrV -O src/Models/
